9bc7c5bcb5cdd8619f9b968dd2721cc5876d2b16
linuxsun <yanyz15@gmail.com>
2018-01-05 17:28:22 +0800

    edit

--- shell/profile.d/java.sh
+++ shell/profile.d/java.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
 JDK_PATH="/application/java/jdk1.8.0_111"
+hava_tty=0
+
+if [ "`tty`" != "not a tty" ]; then
+    have_tty=1
+fi
 
 if [ "$#" -eq 0 ]; then
     VARS=set
@@ -6,23 +12,32 @@ else
     VARS="$1"
 fi
 
+HELP(){
+  echo "source /etc/profile.d/java.sh [set|unset]"
+}
+
 case $VARS in
   set)
-  if [ -z $JAVA_HOME -a -z $CLASSPATH ]; then
+  if [ $have_tty -eq 1 ] && [ -z $JAVA_HOME -a -z $CLASSPATH ]; then
     export JAVA_HOME=$JDK_PATH
     export PATH=$PATH:$JAVA_HOME/bin
     export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
     export JRE_HOME=$JAVA_HOME/jre/bin
     unset JDK_PATH
+    unset hava_tty
   fi
 ;;
   unset)
-  if [ "$VARS" = "unset" ]; then
+  if [ $have_tty -eq 1 ] && [ "$VARS" = "unset" ]; then
     export JAVA_HOME=
     export CLASSPATH=
     export JRE_HOME=
     export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
     unset JDK_PATH
+    unset hava_tty
   fi
+;;
+  *)
+    HELP
 ;;
 esac
