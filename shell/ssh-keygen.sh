5de336e7c1032b6ac708d554c50171c1819e6792
linuxsun <linuxsun@live.cn>
2018-01-12 17:19:55 +0800

    edit

--- shell/ssh-keygen.sh
+++ shell/ssh-keygen.sh
@@ -5,6 +5,6 @@ chmod 700 ~/.ssh
 test -f ~/.ssh/authorized_keys || touch ~/.ssh/authorized_keys
 ssh-keygen -t rsa -b 4096
 
-echo "ssh-copy-id -i ~/.ssh/id_rsa.pub <username>@<host>"
+echo "ssh-copy-id -p 52113 -i ~/.ssh/id_rsa.pub <username>@<host>"
 
 chmod 644 ~/.ssh/authorized_keys
