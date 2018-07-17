89d743072bc538bda974c703e504bb28b6605ebb
linuxsun <linuxsun@live.cn>
2018-07-17 10:37:44 +0800

    edit

--- python/http_code/http_url.py
+++ python/http_code/http_url.py
@@ -2,6 +2,29 @@
 # _*_ coding:utf-8 _*_
 
 class Http_url(object):
+
+    '''
+    滑动HH的值,可调整监控频率.
+    每间隔 1,2,6,60 秒钟,运行一次http状态码扫描. 
+    HH=60;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time 
+    HH=120;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
+    HH=360;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
+    HH=3600;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
+    钉钉群自定义机器人（每个机器人每分钟最多发送20条），支持文本（text）、连接（link）、markdown三种消息类型！
+
+    $ pip install DingtalkChatbot
+    $ python
+    >>> from dingtalkchatbot.chatbot import DingtalkChatbot
+    >>> webhook='https://oapi.dingtalk.com/robot/send?access_token=XYZ'
+    >>> xianding=DingtalkChatbot(webhook)
+    >>> xianding.send_text(msg='test_group_message...', is_at_all=False)
+        {u'errcode': 0, u'errmsg': u'ok'}
+    >>> quit()
+    $ 
+
+    https://github.com/zhuifengshen/DingtalkChatbot
+    '''
+
     HH=3600;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
     access_token='XYZ'
     rob_url='https://oapi.dingtalk.com/robot/send?access_token=' + access_token
@@ -27,29 +50,3 @@ class Http_url(object):
 if __name__ == '__main__':
     url=Http_url()
 
-
-'''
-    HELP: 
-
-    滑动HH的值,可调整监控频率.
-    每间隔 1,2,6,60 秒钟,运行一次http状态码扫描. 
-    HH=60;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time 
-    HH=120;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
-    HH=360;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
-    HH=3600;MM=60;SS=(HH/MM);sleep_time=SS;sleep_time
-    钉钉群自定义机器人（每个机器人每分钟最多发送20条），支持文本（text）、连接（link）、markdown三种消息类型！
-
-    SHELL:
-    $pip install DingtalkChatbot
-    $python
-    >>> from dingtalkchatbot.chatbot import DingtalkChatbot
-    >>> webhook='https://oapi.dingtalk.com/robot/send?access_token=XYZ'
-    >>> xianding=DingtalkChatbot(webhook)
-    >>> xianding.send_text(msg='test_group_message...', is_at_all=False)
-{u'errcode': 0, u'errmsg': u'ok'}
-    >>> quit()
-    $
-
-    #https://github.com/zhuifengshen/DingtalkChatbot
-
-'''
