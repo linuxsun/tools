570aa86695d48f7ced801e719eedbc9c153f471a
Your Name <you@example.com>
2019-01-12 12:24:02 +0800

    edit

--- shell/fdisk_lvms.sh
+++ shell/fdisk_lvms.sh
@@ -4,8 +4,10 @@ DEV="$1"
 DATA="/var/lib/docker"
 LOCK="/var/run/fdisk_lvm.lock"
 FSTAB="/etc/fstab"
+fstatus=0
+test -f $LOCK && fstatus=0 || fstatus=1
 
-if [[ $# -eq 1 ]] && [[ -f $LOCK ]]; then
+if [[ $# -eq 1 ]] && [[ $fstatus -eq 1 ]]; then
 
   pvcreate $DEV
   vgcreate docker $DEV
@@ -16,16 +18,16 @@ if [[ $# -eq 1 ]] && [[ -f $LOCK ]]; then
 
   mkfs -t xfs -n ftype=1 /dev/docker/docker
   xfs_info $DATA |grep 'ftype=1'
-
   test -d $DATA || mkdir -p $DATA
   mount /dev/mapper/docker-docker $DATA
-  
   grep '/dev/mapper/docker-docker' $FSTAB || echo "/dev/mapper/docker-docker /var/lib/docker   xfs defaults 0 0" >> $FSTAB
-
+  if [ $0 ]; then
+    touch $LOCK
+  fi
   chmod -x $0
-
 else
-  echo "$0 /dev/sdaX"
+  echo """1. Delete $LOCK
+2. $0 /dev/sdaX"""
 
 fi
 
