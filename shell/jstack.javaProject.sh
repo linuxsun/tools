b2a36dd16b22a4b5315284c3cb09080660eb97bd
Your Name <you@example.com>
2019-06-03 15:47:39 +0800

    edit

--- shell/jstack.javaProject.sh
+++ shell/jstack.javaProject.sh
@@ -1,41 +1,78 @@
 #!/usr/bin/env bash
 
 which jstack >/dev/null && : || exit 0
-
-# etermconfig=[ etermconfig | tomcat | server-rpc-project.jar | ... | All JAVA projects ]
-projectName='etermconfig' ; Site="${projectName}2"
 Count=0 ; Break=5 ; waitTime=10
 maxSleepCount=1000 ; minFreeMem=1024
-#maxSleepCount=10 ; minFreeMem=1024
+projectName='tomcat' ; Site="${projectName}A"
+# projectName=("etermconfig" | "tomcat" | "server-rpc-project.jar" \
+# | "..." | "All JAVA projects")
+
+if [ $# -eq 1 ];then
+    projectName="$1" ; Site="${projectName}A"
+else
+    projectName="${projectName}"
+fi
+
+Help () {
+    echo "$0 [tomcat|server-rpc-project.jar|...|All_Java_Projects]"
+}
 
 Dump () {
     whoAmi=`whoami`
-    userPid=$(ps -ef |grep java|grep "$projectName"|awk '{print $1,$2}')
-    runPid=${userPid##*\ }
-    runUser=${userPid%%\ *}
-    if [ "$whoAmi" = "$runUser" ]; then
-      Time=$(date +%s) ; Dir=$0 ; Dir=`dirname $Dir`
-      if [ $Dir = '.' ]; then
-          Dir="./" 
-      fi
-      if [ -n "$runPid" ]; then
-          jstack $runPid >> $Dir/${Site}.pid.$runPid.log
-          echo ">>>$Time" >> $Dir/${Site}.pid.$runPid.log
-      fi
+    userPid=(`ps -ef |grep java|grep "$projectName" |grep -Ev \
+"(grep|$0)" |awk '{print $1,$2}'`)
+    #runPid=${userPid##*\ } ; runUser=${userPid%%\ *}
+    if [ ${#userPid[@]} -ge 2 ]; then
+        runUser="${userPid[0]}"
+        runPid=${userPid[1]}
+        if [ $runPid -le 1 ] || [ -z "$runUser" ]; then
+            echo "runPid:<=1,runUser:null"
+            exit 0
+        fi
+    else
+       echo "userPid:null"
+       Help
+       exit 0
+    fi
+    if [ "$whoAmi" = "$runUser" ] && [ -n $runPid ] ; then
+        Time=$(date +%s) ; Dir=$0 ; Dir=`dirname $Dir`
+        jstack $runPid >> $Dir/${Site}.pid.${runPid}.log ; ret=$?
+        if [ $ret -ne 0 ];then
+            break 
+        fi
+        echo "jstack $runPid >> $Dir/${Site}.pid.${runPid}.log"
+        echo ">>>$Time" >> $Dir/${Site}.pid.${runPid}.log
    else
-      echo "当前用户${whoAmi},并非运行该java项目${Site}的用户."
-      #exit 0
+      echo "当前用户${whoAmi},并非运行该java项目${projectName}的用户."
+      exit 0
    fi
 }
 
 Check () {
-    sleepCount=$(top -b -n1 |head -10|grep sleeping| awk '{print $6}')
+    RunSleepCount=(`top -b -n1 |head -10|grep -E '(sleeping|running)'\
+ | awk '{print $4,$6}'`)
+    if [ ${#RunSleepCount[@]} -eq 2 ]; then
+        running=${RunSleepCount[0]}
+        sleeping=${RunSleepCount[1]}
+        sleepCount=$(( running + sleeping ))
+    else
+        sleepCount=0
+    fi
     freeMem=$(free -m|grep Mem|awk '{print $4}')
 }
 
+outputDict () {
+echo -e "{"
+echo -e "    \"Count\":$Count,"
+echo -e "    \"Break\":$Break,"
+echo -e "    \"sleepCount\":$sleepCount,"
+echo -e "    \"maxSleepCount\":$maxSleepCount,"
+echo -e "    \"freeMem\":$freeMem,"
+echo -e "    \"minFreeMem\":$minFreeMem"
+echo -e "}"
+}
+
 Main () {
-  #self=$0
-  #self=${self##*/}
   while [ $Count -le $Break ]
   do
     Check
@@ -45,8 +82,7 @@ Main () {
         Dump
         Count=$(( Count += 1 ))
     fi 
-  echo ">>> Count:$Count Break:$Break sleepCount:$sleepCount \
-maxSleepCount:$maxSleepCount freeMem:$freeMem minFreeMem:$minFreeMem"
+  outputDict
   sleep $waitTime
   done
 }
@@ -54,5 +90,6 @@ maxSleepCount:$maxSleepCount freeMem:$freeMem minFreeMem:$minFreeMem"
 Main
 
 # chmod +x /etc/rc.d/rc.local
-# echo "su - admin -c 'nohup /home/admin/jstack.javaProject.sh >/dev/null 2>&1 &'" >> /etc/rc.local
+# echo "su - admin -c 'nohup /home/admin/jstack.javaProject.sh \
+# >/dev/null 2>&1 &'" >> /etc/rc.local
 
