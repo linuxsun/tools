7f5d9df7c50ac493fd06f549b4aec656a69b57e2
Your Name <you@example.com>
2020-05-21 15:47:45 +0800

    edit

---
+++ shell/jenkins_shell_ssh_restart_apps.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+dHost='1.2.3.4'
+port='22'
+dUser='admin'
+appName="server-rpc-myapp-*-SNAPSHOT.jar"
+appName=$(ls ${WORKSPACE}/$appName|tail -n -1)
+echo "$appName"
+appName=$(echo ${appName##*/})
+destDir="/tmp/rpc"
+
+echo "send: ${appName}"
+scp "-P $port" ${WORKSPACE}/$appName $dUser@$dHost:$destDir/$appName
+
+echo "restart: ${appName}"
+myPID=$(ssh -p $port $dUser@$dHost "ps -ef |grep java |grep $appName|grep -v grep |awk '{print \$2}'")
+for pid in ${myPID} ; do
+  ssh -p $port $dUser@$dHost "ps -f -p $pid >/dev/null 2>&1 && kill -9 $pid || echo 'pid null'"
+done
+ssh -p $port $dUser@$dHost "nohup java -jar $destDir/$appName >/dev/null 2>&1 &"
+
